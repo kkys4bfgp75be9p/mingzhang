9fa02aa7957d6151bec4e0425e4598148fef9ce7
Ming Zhang <zhangming0305@hotmail.com>
2014-11-06 18:17:56 +0100

    fix port in production

--- keystone.js
+++ keystone.js
@@ -36,14 +36,17 @@ keystone.import('models');
 // bundled templates and layouts. Any runtime locals (that should be set uniquely
 // for each request) should be added to ./routes/middleware.js
 
-console.log(keystone.get('env'));
 
 keystone.set('locals', {
 	_: require('underscore'),
 	env: keystone.get('env'),
+  port: process.env.PORT || 3000,
 	utils: keystone.utils,
 	editable: keystone.content.editable
 });
+
+console.log(keystone.get('env'));
+console.log(keystone.get('port'));
 
 // Load your project's Routes
 
